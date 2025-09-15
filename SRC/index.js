67eec153b57c592148ecfdecc997107d985b7bcb
Manuel Fernández <manuel.fernandez@mercoguadiana.net>
2025-09-15 10:23:36 +0200

    Update interface

--- SRC/index.js
+++ SRC/index.js
@@ -1,5 +1,5 @@
 const apiGob = "https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/";
-let container = document.querySelector(".container");
+const container = document.getElementById("container");
 
 // IDs de municipios que quieres consultar
 const municipios = ["717", "732"];
@@ -55,45 +55,58 @@ async function getPetrolStations() {
     });
   }
 
-  gasofas.forEach((gasolinera) => {
-    let div = document.createElement("div");
+gasofas.forEach((gasolinera) => {
+  let card = document.createElement("div");
+  const precios = [];
 
-    div.classList.add(
-      "flex",
-      "flex-col",
-      "justify-between",
-      "p-4",
-      "border-2",
-      "border-green-700",
-      "bg-green-300",
-      "text-gray-900",
-      "shadow-lg",
-      "rounded-xl",
-      "transition",
-      "duration-300",
-      "hover:scale-105",
-    );
+  if (gasolinera.price)
+    precios.push(`<span class="">Gasolina 95: <span class="font-bold">${gasolinera.price} €/L&nbsp;</span></span>`);
+  if (gasolinera.priceGasolinaPlus)
+    precios.push(`<span class="">Gasolina 95+: <span class="font-bold">${gasolinera.priceGasolinaPlus} €/L&nbsp;</span></span>`);
+  if (gasolinera.priceDiesel)
+    precios.push(`<span class="">Gasóleo A: <span class="font-bold">${gasolinera.priceDiesel} €/L&nbsp;</span></span>`);
+  if (gasolinera.priceDieselPlus)
+    precios.push(`<span class="">Gasóleo A+: <span class="font-bold">${gasolinera.priceDieselPlus} €/L&nbsp;</span></span>`);
+  if (gasolinera.priceAdBlue)
+    precios.push(`<span class="font-bold text-blue-900">AdBlue: ${gasolinera.priceAdBlue} €/L&nbsp;</span>`);
 
-    div.innerHTML = `
-      
-      <h2 class="mt-4 font-bold text-center">${gasolinera.logo} - ${gasolinera.localidad}</h2>
-      <ul class="space-y-1 p-4">
-        <li class="font-bold text-green-800">Gasolina: ${gasolinera.price ? gasolinera.price + "€/l" : "No disponible"}</li>
-        <li class="font-bold text-green-800">Gasolina Plus: ${gasolinera.priceGasolinaPlus ? gasolinera.priceGasolinaPlus + "€/l" : "No disponible"}</li>
-        <li class="font-bold text-green-900">Gasóleo A: ${gasolinera.priceDiesel ? gasolinera.priceDiesel + "€/l" : "No disponible"}</li>
-        <li class="font-bold text-green-900">Gasóleo A Plus: ${gasolinera.priceDieselPlus ? gasolinera.priceDieselPlus + "€/l" : "No disponible"}</li>
-        <li class="font-bold text-blue-600">AdBlue: ${gasolinera.priceAdBlue ? gasolinera.priceAdBlue + "€/l" : "No disponible"}</li>
-      </ul>
-      <h1 class="text-lg font-semibold mb-2 text-center">${gasolinera.direction}</h1>
-    `;
+card.classList.add(
+  "bg-green-200",
+  "p-4",
+  "text-green-900",
+  "rounded-lg",
+  "shadow-md",
+  "hover:shadow-lg",
+  "hover:scale-105",
+  "transform",
+  "transition-transform",
+  "transition-shadow",
+  "duration-300",
+  "ease-in-out",
+  "w-full",
+  "max-w-md",         // Tamaño base
+  "md:max-w-lg",      // Más grande en pantallas medianas
+  "lg:max-w-xl",      // Aún más grande en pantallas grandes
+  "xl:max-w-2xl",     // Extra grande en pantallas muy grandes
+  "mx-auto",
+  "border-2",
+  "border-green-500"
+);
 
-    div.style.backgroundImage = `url(./src/img/${gasolinera.logo}.png)`;
-    div.style.backgroundSize = "contain";
-    div.style.backgroundRepeat = "no-repeat";
-    div.style.backgroundPosition = "center top";
 
-    container.appendChild(div);
-  });
+  card.innerHTML = `
+    <div class="flex flex-col items-center mb-2">
+      <h2 class="font-bold text-center">${gasolinera.logo} - ${gasolinera.localidad}</h2>
+      <h1 class="text-center font-extralight italic">${gasolinera.direction}</h1>
+    </div>
+    <div class="flex flex-col items-start space-y-1 w-full">
+      ${precios.join("")}
+    </div>
+  `;
+
+  container.appendChild(card);
+});
+
 }
 
 getPetrolStations();
